2043310b8afaca8e427a414a0c509ea30bb8ab8c
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-11 09:55:53 +0200

    Update HT_main.py

--- HT_main.py
+++ HT_main.py
@@ -1,5 +1,12 @@
 import turtle
 
+
+mat1 = input("Enter first material: ")
+mat2 = input("Enter second material: ")
+
+T1 = float(input("Enter the temprature of {}: ".format(mat1)))
+T2 = float(input("Enter the temprature of {}: ".format(mat2)))
+
 wn = turtle.Screen()
 wn.setup(width=1280, height=720)
 wn.bgcolor("black")
@@ -11,7 +18,7 @@ wn.bgcolor("black")
 
 obj1 = turtle.Turtle()
 obj1.hideturtle()
-obj1.color("white")
+obj1.color("dark gray")
 obj1.shape("square")
 obj1.penup()
 obj1.shapesize(10, 40)
@@ -19,7 +26,7 @@ obj1.shapesize(10, 40)
 
 obj2 = turtle.Turtle()
 obj2.hideturtle()
-obj2.color("blue")
+obj2.color("dark blue")
 obj2.shape("square")
 obj2.penup()
 obj2.shapesize(20, 0.1)
@@ -27,19 +34,46 @@ obj2.shapesize(20, 0.1)
 
 obj3 = turtle.Turtle()
 obj3.hideturtle()
-obj3.color("red")
+obj3.color("dark orange")
 obj3.shape("square")
 obj3.penup()
 obj3.shapesize(3, 3)
-
-
 
 obj1.showturtle()
 obj2.showturtle()
 obj3.showturtle()
 
+txt1 = turtle.Turtle()
+txt1.hideturtle()
+txt1.color("white")
+txt1.penup()
+
+txt2 = turtle.Turtle()
+txt2.hideturtle()
+txt2.color("white")
+txt2.penup()
+
+txt1.goto(-250, 140)
+txt1.write(
+    "{}".format(mat1),
+    font=("Courier", 24, "normal")
+)
+
+txt2.goto(200, 140)
+txt2.write(
+    "{}".format(mat2),
+    font=("Courier", 24, "normal")
+)
+
+if T1 > T2:
+    deltaT = T1 - T2
+
+elif T1 < T2:
+    deltaT = T2 - T1
 
 
 while True:
     wn.update()
+
+    obj3.goto(deltaT, 0)
 
