043788586c8d2a40306d8be3cc909d94f3928cb2
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-04-12 08:11:20 +0200

    Update ListOfMaterials.py

--- ListOfMaterials.py
+++ ListOfMaterials.py
@@ -1,5 +1,13 @@
 # materials and their specific heat capacity
 
 materials = {
-    "iron": "440", "steel": "452"
+    "iron": "440", 
+    "steel": "452",
+    "gold": "129",
+    "mercury": "140",
+    "copper": "385",
+    "aluminum": "902",
+    "water": "4197",
+    "air": "1000",
+    "ice": "2003"
 }
