4d2320dab86408e286ebb3a5804f16be26469e80
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-09-15 13:55:53 +0200

    Update ListOfMaterials.py

--- ListOfMaterials.py
+++ ListOfMaterials.py
@@ -1,7 +1,9 @@
-# materials and their specific heat capacity
+# table of specific heat capacities [in Joules per kilogram Kelvin]
 
 materials = {
     "iron": "440", 
+    "aluminium": "887",
+    "copper": "385",
     "steel": "452",
     "gold": "129",
     "mercury": "140",
@@ -9,5 +11,9 @@ materials = {
     "aluminum": "902",
     "water": "4197",
     "air": "1000",
-    "ice": "2003"
+    "ice": "2003",
+    "silver": "236",
+    "tin": "226",
+    "zinc": "389",
+    "sand": "780"
 }
